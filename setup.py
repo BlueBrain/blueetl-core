a7bc990ca7be41d2b1d13c5aad7209141ac41799
Gianluca Ficarelli <gianluca.ficarelli@epfl.ch>
2023-11-22 12:12:09 +0100

    Remove PIP_INDEX_URL

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     url="https://bbpteam.epfl.ch/documentation/projects/blueetl-core",
     project_urls={
         "Tracker": "https://bbpteam.epfl.ch/project/issues/projects/NSETM/issues",
-        "Source": "git@bbpgitlab.epfl.ch:nse/blueetl-core.git",
+        "Source": "https://github.com/BlueBrain/blueetl-core.git",
     },
     license="BBP-internal-confidential",
     install_requires=[
