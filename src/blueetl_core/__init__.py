3ea43a160a7523d49d16352a780968256dcb4cb9
Gianluca Ficarelli <gianluca.ficarelli@epfl.ch>
2023-12-01 17:30:01 +0100

    Prepare for public release

--- src/blueetl_core/__init__.py
+++ src/blueetl_core/__init__.py
@@ -1,6 +1,6 @@
 """BlueETL core transformations."""
+from blueetl_core._version import __version__  # noqa
 from blueetl_core.etl import register_accessors
-from blueetl_core.version import __version__  # noqa
 
 # registration of Pandas accessors
 register_accessors()
