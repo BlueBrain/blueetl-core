d481a649da6eefff4e0ed6a334b3e8c25f8f56cd
Gianluca Ficarelli <gianluca.ficarelli@epfl.ch>
2024-04-08 15:15:52 +0200

    In blueetl_core.parallel.isolated(), execute the function in the same process if BLUEETL_JOBLIB_JOBS is set to 1

--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -9,14 +9,35 @@ def _myfunc(n):
     return n * n
 
 
-@pytest.mark.parametrize("jobs", [1, 3, None])
-def test_run_parallel(jobs):
+@pytest.mark.parametrize(
+    "jobs, jobs_env",
+    [
+        (1, None),
+        (3, None),
+        (None, None),
+        (None, 1),
+        (None, 3),
+    ],
+)
+def test_run_parallel(monkeypatch, jobs, jobs_env):
+    if jobs_env is not None:
+        monkeypatch.setenv("BLUEETL_JOBLIB_JOBS", str(jobs_env))
+    else:
+        monkeypatch.delenv("BLUEETL_JOBLIB_JOBS", raising=False)
     tasks = [test_module.Task(partial(_myfunc, n=i)) for i in range(3)]
     result = test_module.run_parallel(tasks=tasks, jobs=jobs)
     assert result == [0, 1, 4]
 
 
-def test_isolated():
+def test_isolated_in_sub_process(monkeypatch):
+    monkeypatch.delenv("BLUEETL_JOBLIB_JOBS", raising=False)
+    func = test_module.isolated(_myfunc)
+    result = func(n=2)
+    assert result == 4
+
+
+def test_isolated_in_main_process(monkeypatch):
+    monkeypatch.setenv("BLUEETL_JOBLIB_JOBS", "1")
     func = test_module.isolated(_myfunc)
     result = func(n=2)
     assert result == 4
